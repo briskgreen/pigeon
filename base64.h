f5de7f96fd6e5dfa17c09f8c86d9fdf779922e86
brisk <briskgreen@163.com>
2014-01-05 21:13:43 +0800

    添加base64编码解码

--- base64.h
+++ base64.h
@@ -1,11 +1,11 @@
 #ifndef _BASE64_H
 #define _BASE64_H
 
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-unsigned char *base64_encode(const char *src,const int src_len);
+unsigned char *base64_encode(unsigned char *str);
 
-unsigned char *base64_decode(const char *src,const int src_len);
+unsigned char *bae64_decode(unsigned char *code);
 
 #endif
